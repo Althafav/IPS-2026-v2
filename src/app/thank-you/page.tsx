4f91e87374c8d77712b0d8848b62f784669a019d
Althaf Muhammed Umer <61247025+althafMuhammedUmer@users.noreply.github.com>
2025-10-17 11:38:13 +0400

    thankyou page added

---
+++ src/app/thank-you/page.tsx
@@ -0,0 +1,18 @@
+import Section from "@/components/UI/Section";
+import Globals from "@/modules/Globals";
+import React from "react";
+
+export default async function page() {
+  const response = await Globals.KontentClient.item("thank_you_page")
+    .withParameter("depth", "4")
+    .toPromise();
+
+  const pageData = JSON.parse(JSON.stringify(response.item));
+
+  return <div className="">
+    <div className="h-[76px] bg-black"/>
+    <Section className="max-w-4xl mx-auto">
+        <div dangerouslySetInnerHTML={{__html: pageData.content.value}}/>
+    </Section>
+  </div>;
+}
